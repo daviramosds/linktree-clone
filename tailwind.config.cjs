6c51342492dce0fc9e448cf25c89ee4b84d1b331
daviramosds <davirds.dev@gmail.com>
2022-08-20 18:52:40 -0300

    ui

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        semitransparent: 'rgba(255, 255, 255, 0.5)'
+      },
+      backgroundImage: {
+        bg: `url('https://picsum.photos/1280')`
+      }
+    },
+  },
+  plugins: [],
+};
