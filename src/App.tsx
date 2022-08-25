3a0268812a7772f2b24de56aa196440b917dd2e0
daviramosds <davirds.dev@gmail.com>
2022-08-25 15:32:07 -0300

    graphcms + .env

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+import { useEffect } from "react";
 import { Card } from "./components/Card";
+import { client } from "./lib/apollo";
 import "./styles/global.css";
 
+const GET_LINKS_QUERY = gql`
+  query MyQuery {
+    links {
+      id
+      title
+      url
+    }
+  }
+`;
+
+interface LinkInterface {
+  id: string;
+  title: string;
+  url: string;
+}
+
 function App() {
+  const { data, loading, error } = useQuery(GET_LINKS_QUERY);
+
+  if (loading) return "Loading...";
+  if (error) {
+    console.log(error);
+    return <pre>{error.message}</pre>;
+  }
+
+  const { links } = data;
+
   return (
     <div className="bg-bg bg-cover bg-center p-12 h-screen w-screen">
       <main className="h-full mx-auto max-w-[600px] p-10 flex flex-col items-center relative backdrop-filter backdrop-blur-lg bg-semitransparent rounded">
@@ -11,7 +40,9 @@ function App() {
           draggable={false}
         ></img>
         <h4 className="text-base my-4 font-medium">@davirds</h4>
-        <Card title="Github" url={"hello"} />
+        {links.map((link: LinkInterface) => {
+          return <Card key={link.id} title={link.title} url={link.url} />;
+        })}
       </main>
     </div>
   );
